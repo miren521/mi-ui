0cf7ca8f1c1295faf4590fdcf6f60e42feca08bd
miren <2286101414@qq.com>
2026-04-21 11:29:28 +0800

    布局组件ok了

--- packages/m-col/m-col.vue
+++ packages/m-col/m-col.vue
@@ -1,72 +1,55 @@
 <template>
-  <view 
-    :class="[
-      'm-col',
-      span && `m-col__${span}`,
-      offset && `m-col__offset-${offset}`,
-      customClass
-    ]" 
-    :style="colStyle"
-  >
+  <view :class="['m-col', span && 'm-col__' + span, offset && 'm-col__offset-' + offset, customClass]" :style="rootStyle">
+    <!-- 每一列 -->
     <slot />
   </view>
 </template>
+<script lang="ts">
+export default {
+  name: 'm-col',
+  options: {
+    addGlobalClass: true,
+    virtualHost: true,
+    styleIsolation: 'shared'
+  }
+}
+</script>
 
-<script setup lang="ts">
-import { computed, watch, inject, type CSSProperties } from 'vue'
+<script lang="ts" setup>
+import { computed, watch, inject } from 'vue'
 
 interface ColProps {
-  span?: string | number
-  offset?: string | number
+  span?: number
+  offset?: number
   customClass?: string
-  customStyle?: string | object
+  customStyle?: string
 }
 
 const props = withDefaults(defineProps<ColProps>(), {
   span: 24,
   offset: 0,
   customClass: '',
-  customStyle: () => ({})
+  customStyle: ''
 })
 
 // 从父组件注入 row 信息
 const row = inject('m-row', null)
 
-// 计算列样式
-const colStyle = computed(() => {
-  const style: CSSProperties = {}
-  
-  // 获取父组件的 gutter 值
-  const gutter = row?.props.gutter || 0
-  
-  if (gutter) {
-    const gutterValue = typeof gutter === 'string' ? gutter : `${gutter}rpx`
-    style.paddingLeft = gutterValue
-    style.paddingRight = gutterValue
-  }
-  
-  return {
-    ...style,
-    ...(typeof props.customStyle === 'object' ? props.customStyle : {})
-  }
+const rootStyle = computed(() => {
+  const gutter = row ? row.props.gutter || 0 : 0
+  const padding = `${gutter / 2}rpx`
+  const style = gutter > 0 ? `padding-left: ${padding}; padding-right: ${padding};background-clip: content-box;` : ''
+  return `${style}${props.customStyle}`
 })
 
-// 检查 span 和 offset 的值是否合法
 watch([() => props.span, () => props.offset], () => {
   check()
 })
 
 function check() {
   const { span, offset } = props
-  const spanNum = typeof span === 'string' ? parseInt(span) : span
-  const offsetNum = typeof offset === 'string' ? parseInt(offset) : offset
-  
-  if (spanNum < 0 || offsetNum < 0) {
+  if (span < 0 || offset < 0) {
     console.error('[m-ui] warning(m-col): attribute span/offset must be greater than or equal to 0')
-  }
-  
-  if (spanNum > 24 || offsetNum > 24) {
-    console.error('[m-ui] warning(m-col): attribute span/offset must be less than or equal to 24')
   }
 }
 </script>
