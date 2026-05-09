badf7a4cd29d54764e8e46744249223b968167e5
miren <2286101414@qq.com>
2026-05-09 23:47:01 +0800

    图标更新

--- packages/m-checkbox/m-checkbox.vue
+++ packages/m-checkbox/m-checkbox.vue
@@ -82,7 +82,7 @@ const iconValue = computed(() => {
       icon = isChecked.value ? 'check-rectangle-filled' : 'rectangle'
       break
     case 'dot':
-      icon = isChecked.value ? 'circle1' : 'circle'
+      icon = isChecked.value ? 'check-circle-radio-fill' : 'circle'
       break
     case 'button':
       icon = isChecked.value ? 'check' : ''
