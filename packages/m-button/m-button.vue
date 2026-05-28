e47d780637e632b533580484e5889e35d2c20469
miren <2286101414@qq.com>
2026-05-28 15:57:19 +0800

    提交

--- packages/m-button/m-button.vue
+++ packages/m-button/m-button.vue
@@ -9,23 +9,28 @@
       { 'm-button--block': block },
       { 'm-button--disabled': disabled },
       { 'm-button--loading': loading },
+      { 'm-button--icon': isIcon },
       props.customClass
     ]"
     :style="props.customStyle"
     :disabled="disabled"
     @click="handleClick"
   >
-    <div class="m-button__content">
-      <div v-if="loading" class="m-button__loading">
+    <view class="m-button__content">
+      <view v-if="loading" class="m-button__loading">
         <m-icon name="sync" :size="loadingSize" />
-      </div>
-      <div class="m-button__text"><slot /></div>
-    </div>
+      </view>
+      <m-icon v-else-if="icon || cssIcon" custom-class="m-button__icon" :name="icon" :size="displayIconSize" :class-prefix="classPrefix" :css-icon="cssIcon"></m-icon>
+      <view class="m-button__text" v-if="$slots.default || text">
+        <slot>{{ text }}</slot>
+      </view>
+    </view>
   </button>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, useSlots } from 'vue'
+import mIcon from '../m-icon/m-icon.vue'
 
 interface ButtonProps {
   type?: 'primary' | 'success' | 'warning' | 'danger' | 'info'
@@ -38,7 +43,14 @@ interface ButtonProps {
   loadingColor?: string
   customClass?: string
   customStyle?: string | object
+  icon?: string
+  iconSize?: string | number
+  classPrefix?: string
+  cssIcon?: string | boolean
+  text?: string
 }
+
+const slots = useSlots()
 
 const props = withDefaults(defineProps<ButtonProps>(), {
   type: 'primary',
@@ -50,7 +62,12 @@ const props = withDefaults(defineProps<ButtonProps>(), {
   loading: false,
   loadingColor: '',
   customClass: '',
-  customStyle: () => ({})
+  customStyle: () => ({}),
+  icon: '',
+  iconSize: '',
+  classPrefix: 'm-icon',
+  cssIcon: '',
+  text: ''
 })
 
 const emit = defineEmits<{
@@ -66,6 +83,24 @@ const loadingSize = computed(() => {
     default:
       return '16px'
   }
+})
+
+const displayIconSize = computed(() => {
+  if (props.iconSize) {
+    return props.iconSize
+  }
+  switch (props.size) {
+    case 'large':
+      return '22px'
+    case 'small':
+      return '16px'
+    default:
+      return '20px'
+  }
+})
+
+const isIcon = computed(() => {
+  return !slots.default && !props.text && !!(props.icon || props.cssIcon)
 })
 
 function handleClick(event: MouseEvent) {
--- packages/m-icon/m-icon.vue
+++ packages/m-icon/m-icon.vue
@@ -1,6 +1,7 @@
 <template>
   <view @click="handleClick" :class="rootClass" :style="rootStyle">
-    <image v-if="isImage" class="m-icon__image" :src="name"></image>
+    <image v-if="isImage" class="m-icon__image" :src="name" />
+    <text v-else-if="isDef(cssIcon)" :class="`m-icon__css ${customClass}`" :style="cssIconStyle">{{ cssIcon }}</text>
   </view>
 </template>
 
@@ -25,12 +26,14 @@ interface IconProps {
   classPrefix?: string
   customClass?: string
   customStyle?: string
+  cssIcon?: string | boolean
 }
 
 const props = withDefaults(defineProps<IconProps>(), {
   classPrefix: 'm-icon',
   customClass: '',
-  customStyle: ''
+  customStyle: '',
+  cssIcon: ''
 })
 
 const emit = defineEmits(['click', 'touch'])
@@ -55,12 +58,30 @@ const isImage = computed(() => {
   return isDef(props.name) && props.name.includes('/')
 })
 
+const isCssIcon = computed(() => {
+  return isDef(props.cssIcon) && props.cssIcon !== ''
+})
+
 const rootClass = computed(() => {
   const prefix = props.classPrefix
+  if (isCssIcon.value) {
+    return props.customClass
+  }
   return `${prefix} ${props.customClass} ${isImage.value ? 'm-icon--image' : prefix + '-' + props.name}`
 })
 
 const rootStyle = computed(() => {
+  const style: CSSProperties = {}
+  if (props.color && !isCssIcon.value) {
+    style['color'] = props.color
+  }
+  if (props.size && !isCssIcon.value) {
+    style['font-size'] = addUnit(props.size)
+  }
+  return `${objToStyle(style)} ${props.customStyle}`
+})
+
+const cssIconStyle = computed(() => {
   const style: CSSProperties = {}
   if (props.color) {
     style['color'] = props.color
@@ -68,7 +89,7 @@ const rootStyle = computed(() => {
   if (props.size) {
     style['font-size'] = addUnit(props.size)
   }
-  return `${objToStyle(style)} ${props.customStyle}`
+  return objToStyle(style)
 })
 
 function handleClick(event: any) {
